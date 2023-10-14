d0b326a2a96f8c597587806d4ddd930c2712a3a8
Babita <a00280963@mycambrian.ca>
2023-10-14 16:49:29 -0400

    create 9 test cases for splitting the cheque and tip

--- CheckSplitterLib/CheckSplitter.cs
+++ CheckSplitterLib/CheckSplitter.cs
@@ -1,10 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace CheckSplitter
+namespace CheckSplitterLib
 {
     public class CheckSplitter
     {
-     
+        public decimal SplitAmount(decimal totalAmount, int numberOfPeople)
+        {
+            if (numberOfPeople <= 0)
+            {
+                throw new ArgumentException("Number of people must be greater than zero.");
+            }
+
+            return totalAmount / numberOfPeople;
+        }
+
+        public Dictionary<string, decimal> CalculateTipPerPerson(Dictionary<string, decimal> mealCosts, float tipPercentage)
+        {
+            if (mealCosts == null || mealCosts.Count == 0)
+            {
+                throw new ArgumentException("Meal costs dictionary is empty or null.");
+            }
+
+            if (tipPercentage < 0 || tipPercentage > 100)
+            {
+                throw new ArgumentException("Tip percentage must be between 0 and 100.");
+            }
+
+            var totalCost = mealCosts.Values.Sum();
+
+            if (totalCost == 0)
+            {
+                // Handle the case where the total cost is zero to avoid division by zero.
+                return mealCosts.ToDictionary(pair => pair.Key, pair => 0m);
+            }
+
+            var tipPerPerson = new Dictionary<string, decimal>();
+
+            foreach (var entry in mealCosts)
+            {
+                var individualTip = (decimal)(entry.Value / totalCost * (decimal)tipPercentage / 100);
+                tipPerPerson.Add(entry.Key, individualTip);
+            }
+
+            return tipPerPerson;
+        }
+
+        public decimal CalculateTipPerPerson(decimal totalAmount, int numberOfPeople, float tipPercentage)
+        {
+            if (numberOfPeople <= 0)
+            {
+                throw new ArgumentException("Number of people must be greater than zero.");
+            }
+
+            if (tipPercentage < 0 || tipPercentage > 100)
+            {
+                throw new ArgumentException("Tip percentage must be between 0 and 100.");
+            }
+
+            var tipAmount = totalAmount * (decimal)(tipPercentage / 100);
+            return tipAmount / numberOfPeople;
+        }
+        
     }
 }
--- CheckSplitterTests/CheckSplitterTests.cs
+++ CheckSplitterTests/CheckSplitterTests.cs
@@ -1,9 +1,125 @@
-using System;
-using System.Collections.Generic;
-using CheckSplitter;
-// using Xunit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CheckSplitterLib;
 
-public class CheckSplitterTests
+namespace CheckSplitterTests
 {
-  
+    [TestClass]
+    public class CheckSplitterTests
+    {
+        [TestMethod]
+        public void TestSplitAmount()
+        {
+            // Arrange
+            var checkSplitter = new CheckSplitter();
+
+            // Act
+            var splitAmount = checkSplitter.SplitAmount(100, 4);
+
+            // Assert
+            Assert.AreEqual(25, splitAmount);
+        }
+
+        [TestMethod]
+        public void TestSplitAmount_WithZeroTotalAmount()
+        {
+            // Arrange
+            var checkSplitter = new CheckSplitter();
+
+            // Act
+            var splitAmount = checkSplitter.SplitAmount(0, 4);
+
+            // Assert
+            Assert.AreEqual(0, splitAmount);
+        }
+
+        [TestMethod]
+        public void TestSplitAmount_WithNegativeNumberOfPeople()
+        {
+            // Arrange
+            var checkSplitter = new CheckSplitter();
+
+            // Act and Assert
+            Assert.ThrowsException<ArgumentException>(() => checkSplitter.SplitAmount(100, -1));
+        }
+
+        [TestMethod]
+        public void TestCalculateTipPerPerson_WithEmptyMealCosts()
+        {
+            // Arrange
+            var checkSplitter = new CheckSplitter();
+            var mealCosts = new Dictionary<string, decimal>();
+
+            // Act and Assert
+            Assert.ThrowsException<ArgumentException>(() => checkSplitter.CalculateTipPerPerson(mealCosts, 10));
+        }
+
+        [TestMethod]
+        public void TestCalculateTipPerPerson_WithInvalidTipPercentage()
+        {
+            // Arrange
+            var checkSplitter = new CheckSplitter();
+            var mealCosts = new Dictionary<string, decimal> { { "Person1", 50 }, { "Person2", 50 } };
+
+            // Act and Assert
+            Assert.ThrowsException<ArgumentException>(() => checkSplitter.CalculateTipPerPerson(mealCosts, -10));
+        }
+
+        [TestMethod]
+        public void TestCalculateTipPerPerson_WithZeroMealCosts()
+        {
+            // Arrange
+            var checkSplitter = new CheckSplitter();
+            var mealCosts = new Dictionary<string, decimal> { { "Person1", 0 }, { "Person2", 0 } };
+
+            // Act
+            var tipPerPerson = checkSplitter.CalculateTipPerPerson(mealCosts, 10);
+
+            // Assert
+            Assert.AreEqual(0, tipPerPerson["Person1"]);
+            Assert.AreEqual(0, tipPerPerson["Person2"]);
+        }
+
+        [TestMethod]
+        public void TestCalculateTipPerPerson_WithLargeTipPercentage()
+        {
+            // Arrange
+            var checkSplitter = new CheckSplitter();
+            var mealCosts = new Dictionary<string, decimal> { { "Person1", 50 }, { "Person2", 50 } };
+
+            // Act and Assert
+            Assert.ThrowsException<ArgumentException>(() => checkSplitter.CalculateTipPerPerson(mealCosts, 110));
+        }
+
+        [TestMethod]
+        public void TestCalculateTipPerPerson_WithZeroTipPercentage()
+        {
+            // Arrange
+            var checkSplitter = new CheckSplitter();
+            var mealCosts = new Dictionary<string, decimal> { { "Person1", 50 }, { "Person2", 50 } };
+
+            // Act
+            var tipPerPerson = checkSplitter.CalculateTipPerPerson(mealCosts, 0);
+
+            // Assert
+            Assert.AreEqual(0, tipPerPerson["Person1"]);
+            Assert.AreEqual(0, tipPerPerson["Person2"]);
+        }
+
+        [TestMethod]
+        public void TestCalculateTipPerPerson_WithDecimalTipPercentage()
+        {
+            // Arrange
+            var checkSplitter = new CheckSplitter();
+            var mealCosts = new Dictionary<string, decimal> { { "Person1", 50 }, { "Person2", 50 } };
+
+            // Act
+            var tipPerPerson = checkSplitter.CalculateTipPerPerson(mealCosts, 10.5f);
+
+            // Assert
+            Assert.AreEqual(0.0525m, tipPerPerson["Person1"]);
+            Assert.AreEqual(0.0525m, tipPerPerson["Person2"]);
+        }
+
+
+    }
 }
